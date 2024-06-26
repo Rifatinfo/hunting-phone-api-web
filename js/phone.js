a4b6388f06cf8c9ab50e42f9860724d6f1ff2133
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-06-26 16:40:51 +0600

    phone.js

---
+++ js/phone.js
@@ -0,0 +1,53 @@
+const loadPhone = async(searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(phones);
+    displayPhones(phones);
+}
+
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phone-container');
+
+    // clear phone container cards before adding new cards 
+    phoneContainer.textContent = '';
+
+    // display only first 12 phones 
+    phones =  phones.slice(0, 12);
+    phones.forEach( phones => {
+        console.log(phones);
+        // 2 create a div 
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
+        // 3 set inner HTML 
+        phoneCard.innerHTML = `
+                    <figure>
+                      <img
+                        src="${phones.image}"
+                        alt="" />
+                    </figure>
+                    <div class="card-body">
+                      <h2 class="card-title">${phones.phone_name}</h2>
+                      <p>If a dog chews shoes whose shoes does he choose?</p>
+                      <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                      </div>
+                    </div>
+                `;
+                // append child 
+                phoneContainer.appendChild(phoneCard);
+
+    });
+}
+
+// handle search button 
+const handleSearch = () => {
+    const searchFiled = document.getElementById('search-filed');
+    const searchText = searchFiled.value;
+    console.log(searchText);
+    loadPhone(searchText);
+} 
+
+
+loadPhone();
+
